899327263f310ebe40ad6ce34214328c0ebd4068
Kasper <kasperflodin@gmail.com>
2024-04-10 23:10:36 +0200

    first update

---
+++ app/src/main/java/com/example/myfirstapplication/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.myfirstapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+//        buttonClickingandLollygagging();
+        inputThyName();
+
+    }
+
+    private void inputThyName() {
+        EditText nameInput = findViewById(R.id.inp_name);
+        Button btn_input = findViewById(R.id.btn_inputbtn);
+        btn_input.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameInput.getText().toString();
+                Toast.makeText(getApplicationContext(), "" + name, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+//    private void buttonClickingandLollygagging() {
+//        Button btn = findViewById(R.id.btn_mybutton);
+//        btn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                if (btn.getText() == "I dare you to click me again!!!")
+//                    btn.setText("You did it again didnt you??");
+//                else
+//                    btn.setText("I dare you to click me again!!!");
+//            }
+//        });
+//    }
+}
